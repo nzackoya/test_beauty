58a9b6d63d19bbd95c76eb5051e677c7cebf85f2
Nerses <nerses@zakoyan.com>
2025-08-06 21:34:34 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:grouped_list/grouped_list.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(MyApp(repository: SessionRepository()));
+}
+
+class Session {
+  Session(this.date, this.customer, this.amount);
+
+  final DateTime date;
+  final String customer;
+  final int amount;
+}
+
+class SessionRepository {
+  // Dates: 1 <= n <= 365
+  // Clients: 1 <= m <= 1000
+  Future<List<Session>> fetch() {
+    return Future.value([
+      Session(DateTime(2025, 7, 1), 'Customer A', 100),
+      Session(DateTime(2025, 7, 1), 'Customer B', 200),
+      Session(DateTime(2025, 7, 1), 'Customer C', 300),
+      Session(DateTime(2025, 7, 2), 'Customer D', 300),
+      Session(DateTime(2025, 7, 2), 'Customer E', 300),
+      Session(DateTime(2025, 7, 2), 'Customer F', 300),
+      Session(DateTime(2025, 7, 2), 'Customer A', 300),
+      Session(DateTime(2025, 7, 3), 'Customer B', 300),
+    ]);
+  }
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key, required this.repository});
+
+  final SessionRepository repository;
+
+  final formattedDate = DateFormat('EEEE, MMMM d, yyyy');
+
+  final mainColor = Color(0xffEDECEE);
+
+  final titleFont = TextStyle(fontWeight: FontWeight.bold, fontSize: 12);
+  final totalFont = TextStyle(fontSize: 12);
+  final amountFont = TextStyle(fontSize: 10);
+  final customerFont = TextStyle(fontSize: 13);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        backgroundColor: mainColor,
+        body: SafeArea(
+          child: FutureBuilder(
+            future: SessionRepository().fetch(),
+            builder: (context, snapshot) {
+              final elements = snapshot.data ?? [];
+              return GroupedListView<Session, String>(
+                padding: EdgeInsets.all(10),
+                elements: elements,
+                groupBy: (element) => formattedDate.format(element.date),
+                groupSeparatorBuilder: (String groupByValue) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      left: 10,
+                      right: 10,
+                      bottom: 3,
+                      top: 12,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Date', style: titleFont),
+                        Text(groupByValue, style: titleFont),
+                      ],
+                    ),
+                  );
+                },
+                separator: Divider(height: 1, color: mainColor),
+                groupItemBuilder: (context, session, groupStart, groupEnd) {
+                  final rowItem = Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(groupStart ? 8.0 : 0),
+                        topRight: Radius.circular(groupStart ? 8.0 : 0),
+                        bottomLeft: Radius.circular(groupEnd ? 8.0 : 0),
+                        bottomRight: Radius.circular(groupEnd ? 8.0 : 0),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(session.customer, style: customerFont),
+                        Text(session.amount.toString(), style: amountFont),
+                      ],
+                    ),
+                  );
+                  return !groupEnd
+                      ? rowItem
+                      : Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            rowItem,
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 10,
+                                vertical: 5,
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text('Total', style: totalFont),
+                                  Text(
+                                    elements
+                                        .where(
+                                          (val) => val.date == session.date,
+                                        )
+                                        .fold<int>(
+                                          0,
+                                          (initial, current) =>
+                                              initial + current.amount,
+                                        )
+                                        .toString(),
+                                    style: totalFont,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        );
+                },
+                itemBuilder: (context, Session element) =>
+                    Text(element.customer),
+                // itemComparator: (item1, item2) =>
+                //     item1.date.compareTo(item2.date), // optional
+                // useStickyGroupSeparators: true, // optional
+                // floatingHeader: true, // optional
+                // order: GroupedListOrder.DESC, // optional
+                // footer: Text("Widget at the bottom of list"), // optional
+                sort: false,
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
